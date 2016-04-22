0e0445973bba1b17b7e1fa92de964140802df377
Mehta <@purdue.edu>
2016-04-22 10:39:28 -0400

    Deleted URL

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var host = "localhost";
 var port = 3030;
 
 var cloudant = {
-	url : "https://04fea742-a58c-4952-9d62-e7a493b49f95-bluemix.cloudant.com/categories_with_friends_hiscores/_all_docs" 
+//	url : "https://04fea742-a58c-4952-9d62-e7a493b49f95-bluemix.cloudant.com/categories_with_friends_hiscores/_all_docs" 
+	url : "<url>" //todo
 };
 if (process.env.hasOwnProperty("VCAP_SERVICES")) {
 	var env = JSON.parse(proces.env.VCAP_SERVICES);
