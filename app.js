ab074cd50f3ded05e393792067898a4af8ee7e0e
Guna Kondapaneni <guna.kondapaneni@gmail.com>
2016-04-22 18:02:39 +0000

    testing things

--- app.js
+++ app.js
@@ -3,81 +3,47 @@
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
-var express = require('express');
+
 	
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
 
+var express = require('express');
 // create a new express server
 var app = express();
 
 
-// serve the files out of ./public as our main files
+// serve the files out of ./views as our main files
 app.use(express.static(__dirname + '/views'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
 
-//adding database stuff
-var cloudant = {
-	//url : "https://04fea742-a58c-4952-9d62-e7a493b49f95-bluemix.cloudant.com/categories_with_friends_hiscores/_all_docs" //todo
-	url : "https://04fea742-a58c-4952-9d62-e7a493b49f95-bluemix:018fdcefebbdb083c91e3dad0ba1ae8812da71cba1225d63555e200efb61cce4@04fea742-a58c-4952-9d62-e7a493b49f95-bluemix.cloudant.com"
-};
-
-
-if (process.env.hasOwnProperty("VCAP_SERVICES")) {
-	var env = JSON.parse(process.env.VCAP_SERVICES);
-	var host = process.env.VCAP_APP_HOST;
-	var port = process.env.VCAP_APP_PORT;
-
-	cloudant = env['cloudantNoSQLDB'][0].credentials;
-}
-
-var nano = require('nano')(cloudant.url);
-var db = nano.db.use('categories_with_friends_hiscores');
-
-//new code added
-app.get('/hiscores', function(request, response) {
-  db.view('top_scores', 'top_scores_index', function(err, body) {
-  if (!err) {
-    var scores = [];
-      body.rows.forEach(function(doc) {
-        scores.push(doc.value);		      
-      });
-      response.send(JSON.stringify(scores));
-    } else {
-    	response.send("I'm a failure");
-    }
-  });
-});
-
-app.get('/save_score', function(request, response) {
-  var name = request.query.name;
-  var score = request.query.score;
-
-  var scoreRecord = { 'name': name, 'score' : parseInt(score), 'date': new Date() };
-  db.insert(scoreRecord, function(err, body, header) {
-    if (!err) {       
-      response.send('Successfully added one score to the DB');
-    }
-  });
-});
-//end of new code and database code
-
-
+//this is the 
 app.get('/', function(req, res){
   res.render('hiscores.jade', {title: 'Hiscores'});
+  res.send('HI there');
 });
 
-app.get('/play', function(req, res){	
-  res.render('main.jade', {title: 'Guess the Word'});
+//this is where will retrieve the db data
+app.get('/hi', function(req, res) {
+	//res.render('hiscores.jade', {title: 'scores'});
+	res.send('HI there');
 });
+
+//ths i 
+app.get('/play', function(req, res){	
+    res.render('main.jade', {title: 'Guess the Word'});
+	//res.send('nothing works');
+});
+
+
 
 // start server on the specified port and binding host
-app.listen(port, '0.0.0.0', function() {
+app.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
