25a22bb53d90786a093a912b64a84981264e1d20
Guna Kondapaneni <guna.kondapaneni@gmail.com>
2016-04-23 20:13:28 +0000

    More UI things. Having trouble displaying the time.

--- views/js/main.js
+++ views/js/main.js
@@ -1,24 +1,47 @@
 
 //the submit function
-
 function submit() {
 	//alert("Submitted!");
 	var thing = document.getElementById("one").value;
 	document.getElementById("five").value = thing;
 }
-
-function checkTime(time) {
-	
-	var updateTime = (new Date()).getMilliseconds();
-	while((updateTime - time) < 120000) {
-		
-	}
+function checkTime() {
+	var d = new Date();
+	var currTime = d.getMilliseconds();
+	document.getElementById("score").innnerHTML = d.toLocaleTimeString();
 	
 }
+
+function start() {
+	//hide the button and the title
+	document.getElementById("ready").style.display = "none";
+	document.getElementById("cate").style.display = "none";
+
+	document.getElementById("score").style.display = "block";
+
+	
+	//display the relevent things
+	//$("#score").fadeIn();
+	//$("#power").fadeIn();
+	//$("#inputting").fadeIn();
+	
+	
+		
+	var inner = document.getElementById("score");
+	var d = new Date();
+	inner.innnerHTML = d.toLocaleTimeString();	
+	
+	console.log(d.toLocaleTimeString()+" "+ document.getElementById("score").innnerHTML);
+
+
+}
+
+
 
 //this gets the random letter from the server
 function getLetter() {
 	
 }
-var currTime = (new Date()).getMilliseconds();
-checkTime(currTime);
+
+
+
