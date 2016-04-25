3a4b4011430d0f86ee04f9113b2dc2ebba7cf263
Guna Kondapaneni <guna.kondapaneni@gmail.com>
2016-04-25 21:41:32 +0000

    Working?

--- app.js
+++ app.js
@@ -26,6 +26,17 @@ var db = null;
 
 var mysql = require('mysql');
 
+db = mysql.createConnection(
+{
+	host: 'us-cdbr-iron-east-03.cleardb.net',
+	port: '3306',
+	user: 'b949f5a82f36fb',
+	password: '3b81693c',
+	database: 'ad_3063a2f467afe38'
+});
+
+var letter;
+var categories;
 
 
 // serve the files out of ./views as our main files
@@ -34,6 +45,7 @@ app.use(express.static(__dirname + '/views'));
 
 //this is the 
 app.get('/', function(req, res){
+	db.connect();
 	var ty = 'Hiscores';
   	res.render('hiscores.jade', {title: ty});
   	console.log('What even');
@@ -42,14 +54,49 @@ app.get('/', function(req, res){
 //this is where will retrieve the db data
 app.get('/check', function(req, res) {
 	
-	console.log("Body of req "+req.query.one);
 	
 	//concat the data
 	var sends = req.query.one +"|"+req.query.two+"|"+req.query.three+"|"+req.query.four+"|"+req.query.five;
+	console.log("Body of req "+sends);
+	//split to array of strings
 	sends = sends.split("|");
 	
+	//get computer outputs
+	var comp = "blank|";
+	
+	//DB queries
+	
+	//split the categories
+	var cates = categories.split("|");
+	
+	//generate random number
+//	var index = Math.floor(Math.random()*25) + 1;
+	console.log(letter+"  "+cates[1]);
+
+//	db.query('SELECT fruits FROM word WHERE id = 1', function(err, rows)
+//	{
+//	
+//		console.log('stuff');
+//		if(cates[1] == 'fruits')
+//			console.log('row content: '+rows[0].fruits[0]);
+//								
+//		var tmp = rows[0].fruits;
+//		
+//		if(tmp.indexOf(',') != -1)
+//			tmp = tmp.substr(0, tmp.indexOf(','));
+//		
+//		comp = comp + tmp;
+//	});
+
+    
+	console.log(comp);
+	
+	//TODO split into an array and send to client via res.render()
+	var sendC = comp.split("|");
+	
 	//send a full string
-	res.render('client.jade', {answer: sends});
+	res.render('client.jade', {answer: sends, cpu: sendC});
+	
 	
 	
 });
@@ -61,29 +108,30 @@ app.get('/hi', function(req, res) {
 	
 });
 
+
 //this is where the meat is 
 app.get('/play', function(req, res){	
 
-db = mysql.createConnection(
-{
-	host: 'us-cdbr-iron-east-03.cleardb.net',
-	port: '3306',
-	user: 'b949f5a82f36fb',
-	password: '3b81693c',
-	database: 'ad_3063a2f467afe38'
-});
-
-db.connect();
 
 //get random category
 var high = 2;
 var low = 1;
 var cat = Math.floor(Math.random() * (high - low) + low);
 
+//generate a letter
+var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+letter = Math.floor( Math.random() * 26) ;
+
+
+//DB Connection
+
+
+
 var value1 = "nothing|";
 db.query('SELECT name FROM categories WHERE id = 1', function(err, rows)
 {
-	//if (err) throw err;
+
 	console.log('1');
 	console.log('Data received from Db:\n');
 	console.log(rows[0].name);
@@ -94,7 +142,7 @@ db.query('SELECT name FROM categories WHERE id = 1', function(err, rows)
 
 db.query('SELECT name FROM categories WHERE id = 2', function(err, rows)
 {
-	//if (err) throw err;
+
 	console.log('2');
 	console.log('Data received from Db:(value)\n');
 	console.log(value1);
@@ -102,16 +150,9 @@ db.query('SELECT name FROM categories WHERE id = 2', function(err, rows)
 	value1 = value1+"|"+ rows[0].name;
 	console.log(value1);
 	
-
-	///res.end(value1);
-
 });
 db.query('SELECT name FROM categories WHERE id = 3', function(err, rows)
 {
-	//if (err) throw err;
-//	console.log('2');
-//	console.log('Data received from Db:(value)\n');
-//	console.log(value1);
 
 	value1 = value1+"|"+ rows[0].name;
 	console.log(value1);
@@ -128,19 +169,17 @@ db.query('SELECT name FROM categories WHERE id = 4', function(err, rows)
 	value1 = value1+"|"+ rows[0].name;
 	console.log(value1);
 	
+	//store the categories
+	categories= value1;
+	
 	var val = value1.split("|");
 	
 	//send the data to Main
-	res.render('main.jade', {title: 'Guess the Word', value: val});
+	res.render('main.jade', {title: 'Guess the Word', value: val, letr: alpha.charAt(letter-1)});
 
 	///res.end(value1);
 
 });
-
-
-
-//end all the things
-db.end({timeout: 60000});
 
 
 	
---
+++ views/js/client.js
@@ -0,0 +1,15 @@
+
+//TODO display the score
+function score()
+{
+	//calculate the score 
+	var score = 0;
+	//parse the elements in HTML
+	
+	
+	//then compare them
+	
+	//display the score column
+	$(".scores").fadeIn();
+	$("#playagain").fadeIn();
+}
